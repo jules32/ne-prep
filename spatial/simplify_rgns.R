caffebd1cc766ed6416a9d4b5540e435216a983b
Jamie Afflerbach <afflerbach@nceas.ucsb.edu>
2018-07-18 12:34:54 -0700

    created final ohi ne regions. involved removal of unnecessary shapefiles, and creation of new ones

--- spatial/simplify_rgns.R
+++ spatial/simplify_rgns.R
@@ -3,12 +3,14 @@
 library(rgdal)
 library(rgeos)
 
-rgns       <- readOGR(dsn = paste0(path.expand(dir_git),'/spatial'),layer = 'ne_ohi_rgns', verbose = FALSE) #need to use path.expand because readOGR does not read '~'
+dir_git <- "~/github/ne-prep"
+
+rgns       <- readOGR(dsn = paste0(path.expand(dir_git),'/spatial/shapefiles'),layer = 'ne_ohi_rgns', verbose = FALSE) #need to use path.expand because readOGR does not read '~'
 
 #gSimplify
 
 rgns_simp <- gSimplify(rgns, tol = 100, topologyPreserve = TRUE)
-rgns_simp <- SpatialPolygonsDataFrame(rgns_simp,data=as.data.frame(rgns@data))
-writeOGR(rgns_simp,dsn = 'spatial/shapefiles',layer = 'ne_ohi_rgns_simp',driver="ESRI Shapefile")
+rgns_simp <- SpatialPolygonsDataFrame(rgns_simp, data = as.data.frame(rgns@data))
+writeOGR(rgns_simp, dsn = 'spatial/shapefiles', layer = 'ne_ohi_rgns_simp', driver="ESRI Shapefile", overwrite_layer = T)
 
 # Removing small polygons
